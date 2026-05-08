96d23a6dd1bb4b8fe4a72fe971277bddd1e5e7a5
Zuha Azhar <zuhazhar19@gmail.com>
2026-05-08 18:53:07 +0500

    Refactor Vehicle class to include vehicle types and colors

--- code/src/movingobstacle.cpp
+++ code/src/movingobstacle.cpp
@@ -1,14 +1,33 @@
 #include "movingobstacle.h"
+#include "constants.h"
 
-Vehicle::Vehicle(float x, float y, float speed, int direction, float size)
-    : Obstacle(x, y, size * 2, size, Color::Yellow, speed) {
-    this->direction = direction;
+// size and color per vehicle type
+sf::Vector2f Vehicle::sizeFor(VehicleType t) {
+    switch (t) {
+        case VehicleType::BIKE:     return sf::Vector2f(TILE * 0.8f,  TILE * 0.5f);
+        case VehicleType::RICKSHAW: return sf::Vector2f(TILE * 1.5f,  TILE * 0.7f);
+        case VehicleType::DUMPER:   return sf::Vector2f(TILE * 2.5f,  TILE * 0.85f);
+        default:                    return sf::Vector2f(TILE,          TILE);
+    }
 }
 
-void Vehicle::update(float dt) {
-    shape.move({speed * direction * dt, 0.f});
+sf::Color Vehicle::colorFor(VehicleType t) {
+    switch (t) {
+        case VehicleType::BIKE:     return sf::Color(255, 100, 100);   // red
+        case VehicleType::RICKSHAW: return sf::Color(255, 200, 0);     // yellow
+        case VehicleType::DUMPER:   return sf::Color(100, 100, 255);   // blue
+        default:                    return sf::Color::White;
+    }
+}
 
-    // wraparound when off screen
+Vehicle::Vehicle(float x, float y, float speed, int direction, VehicleType type, float windowWidth)
+    : Obstacle(x, y, sizeFor(type).x, sizeFor(type).y, colorFor(type), speed, direction),
+      windowWidth(windowWidth), vehicleType(type)
+{}
+
+void Vehicle::update(float dt) {
+    shape.move(sf::Vector2f(speed * direction * dt, 0.f));
+
     float x = shape.getPosition().x;
     if (direction == 1 && x > windowWidth + 10.f)
         shape.setPosition(sf::Vector2f(-shape.getSize().x, shape.getPosition().y));
