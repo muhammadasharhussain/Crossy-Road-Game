22baa1764f66da3c01aacfa1dbfbf0dfe5bd5bde
Muhammad Ashar <145932531+muhammadasharhussain@users.noreply.github.com>
2026-05-10 02:35:37 +0500

    Refactor Tree and Rock classes to use sprites

--- code/src/staticobstacle.cpp
+++ code/src/staticobstacle.cpp
@@ -1,26 +1,21 @@
 #include "staticobstacle.h"
 
 Tree::Tree(float x, float y)
-    : Obstacle(x, y, 40.f, 40.f, Color(0, 100, 0), 0.f) {
+    : Obstacle(x, y, 40.f, 40.f, "..\\assets\\images\\tree.png", 0.f) {
 }
 
-void Tree::update(float dt) {
-    // static, does nothing
-}
+void Tree::update(float dt) {}
 
 void Tree::draw(RenderWindow& window) {
-    window.draw(shape);
+    window.draw(*sprite);
 }
-
 
 Rock::Rock(float x, float y)
-    : Obstacle(x, y, 40.f, 40.f, Color(105, 105, 105), 0.f) {
+    : Obstacle(x, y, 40.f, 40.f, "..\\assets\\images\\rock.png", 0.f) {
 }
 
-void Rock::update(float dt) {
-    // static, does nothing
-}
+void Rock::update(float dt) {}
 
 void Rock::draw(RenderWindow& window) {
-    window.draw(shape);
+    window.draw(*sprite);
 }
