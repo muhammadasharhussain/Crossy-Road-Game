01d7273a028707e6770c73a23206298025b9d768
Muhammad Ashar <145932531+muhammadasharhussain@users.noreply.github.com>
2026-05-10 02:34:59 +0500

    Refactor Vehicle and Log classes with texture support

--- code/src/movingobstacle.cpp
+++ code/src/movingobstacle.cpp
@@ -1,59 +1,61 @@
 #include "movingobstacle.h"
 #include "constants.h"
 
-// size and color per vehicle type
 sf::Vector2f Vehicle::sizeFor(VehicleType t) {
     switch (t) {
-        case VehicleType::BIKE:     return sf::Vector2f(TILE * 0.8f,  TILE * 0.5f);
-        case VehicleType::RICKSHAW: return sf::Vector2f(TILE * 1.5f,  TILE * 0.7f);
-        case VehicleType::DUMPER:   return sf::Vector2f(TILE * 2.5f,  TILE * 0.85f);
-        default:                    return sf::Vector2f(TILE,          TILE);
+        case VehicleType::BIKE:     return {TILE * 0.8f,  TILE * 0.5f};
+        case VehicleType::RICKSHAW: return {TILE * 1.5f,  TILE * 0.7f};
+        case VehicleType::DUMPER:   return {TILE * 2.5f,  TILE * 0.85f};
+        default:                    return {TILE, TILE};
     }
 }
 
-sf::Color Vehicle::colorFor(VehicleType t) {
+std::string Vehicle::textureFor(VehicleType t) {
     switch (t) {
-        case VehicleType::BIKE:     return sf::Color(255, 100, 100);   // red
-        case VehicleType::RICKSHAW: return sf::Color(255, 200, 0);     // yellow
-        case VehicleType::DUMPER:   return sf::Color(100, 100, 255);   // blue
-        default:                    return sf::Color::White;
+        case VehicleType::BIKE:     return "..\\assets\\images\\bike.png";
+        case VehicleType::RICKSHAW: return "..\\assets\\images\\rickshaw.png";
+        case VehicleType::DUMPER:   return "..\\assets\\images\\dumper.png";
+        default:                    return "..\\assets\\images\\bike.png";
     }
 }
 
 Vehicle::Vehicle(float x, float y, float speed, int direction, VehicleType type, float windowWidth)
-    : Obstacle(x, y, sizeFor(type).x, sizeFor(type).y, colorFor(type), speed, direction),
-      windowWidth(windowWidth), vehicleType(type)
-{}
+    : Obstacle(x, y, sizeFor(type).x, sizeFor(type).y, textureFor(type), speed, direction),
+      windowWidth(windowWidth), vehicleType(type) {
+    objWidth = sizeFor(type).x;
+    applyFlip();
+}
 
 void Vehicle::update(float dt) {
-    shape.move(sf::Vector2f(speed * direction * dt, 0.f));
-
-    float x = shape.getPosition().x;
+    sprite->move({speed * direction * dt, 0.f});
+    float x = sprite->getPosition().x;
     if (direction == 1 && x > windowWidth + 10.f)
-        shape.setPosition(sf::Vector2f(-shape.getSize().x, shape.getPosition().y));
-    else if (direction == -1 && x < -shape.getSize().x - 10.f)
-        shape.setPosition(sf::Vector2f(windowWidth + 10.f, shape.getPosition().y));
+        sprite->setPosition({-objWidth, sprite->getPosition().y});
+    else if (direction == -1 && x < -objWidth - 10.f)
+        sprite->setPosition({windowWidth + 10.f, sprite->getPosition().y});
 }
 
 void Vehicle::draw(RenderWindow& window) {
-    window.draw(shape);
+    window.draw(*sprite);
 }
 
 Log::Log(float x, float y, float speed, int direction, float windowWidth)
-    : Obstacle(x, y, TILE * 2.5f, TILE * 0.7f, sf::Color(139, 90, 43), speed, direction),
-      windowWidth(windowWidth)
-{}
+    : Obstacle(x, y, TILE * 2.5f, TILE * 0.7f,
+        "..\\assets\\images\\log.png", speed, direction),
+      windowWidth(windowWidth) {
+    objWidth = TILE * 2.5f;
+    applyFlip();
+}
 
 void Log::update(float dt) {
-    shape.move(sf::Vector2f(speed * direction * dt, 0.f));
-
-    float x = shape.getPosition().x;
+    sprite->move({speed * direction * dt, 0.f});
+    float x = sprite->getPosition().x;
     if (direction == 1 && x > windowWidth + 10.f)
-        shape.setPosition(sf::Vector2f(-shape.getSize().x, shape.getPosition().y));
-    else if (direction == -1 && x < -shape.getSize().x - 10.f)
-        shape.setPosition(sf::Vector2f(windowWidth + 10.f, shape.getPosition().y));
+        sprite->setPosition({-objWidth, sprite->getPosition().y});
+    else if (direction == -1 && x < -objWidth - 10.f)
+        sprite->setPosition({windowWidth + 10.f, sprite->getPosition().y});
 }
 
 void Log::draw(sf::RenderWindow& window) {
-    window.draw(shape);
+    window.draw(*sprite);
 }
