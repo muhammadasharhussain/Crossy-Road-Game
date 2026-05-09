a1cdfe85ac5a91077942b5c017be6e8064d1e99d
Muhammad Ashar <145932531+muhammadasharhussain@users.noreply.github.com>
2026-05-10 02:31:26 +0500

    Refactor Obstacle class with texture support
    Updated Obstacle class to include texture handling and copy constructor.

--- code/include/obstacle.h
+++ code/include/obstacle.h
@@ -1,23 +1,28 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <string>
 using namespace sf;
 
 class Obstacle {
 public:
-    Obstacle(float x, float y, float w, float h, Color color, float speed, int dir=1);
+    Obstacle(float x, float y, float w, float h, const std::string& texturePath, float speed, int dir=1);
+    Obstacle(const Obstacle& other);
+    virtual ~Obstacle();
 
-    virtual void update(float dt) = 0;       // pure virtual
-    virtual void draw(RenderWindow& window) = 0; // pure virtual
-    virtual Obstacle* clone() const = 0; 
+    virtual void update(float dt) = 0;
+    virtual void draw(RenderWindow& window) = 0;
+    virtual Obstacle* clone() const = 0;
 
     void setPosition(float x, float y);
     FloatRect getBounds();
     bool isOffScreen();
     float getSpeedX();
     void setSpeed(float s) { speed = s; }
+    void applyFlip();
 
 protected:
-    RectangleShape shape;
+    Texture texture;
+    Sprite* sprite;
     float speed;
-    int direction; // +1 right, -1 left
+    int direction;
 };
